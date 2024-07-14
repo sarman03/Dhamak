d0abd3820aba097081e1588dd76cb23bcc71a4ed
Sarmanpreet singh <131851235+sarman03@users.noreply.github.com>
2024-07-14 19:49:21 +0000

    feed

--- dhamak/src/app/page.tsx
+++ dhamak/src/app/page.tsx
@@ -1,7 +1,9 @@
-
+import HeroSection from "../components/HeroSection";
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">home feed</h1>
+    <div>
+    <HeroSection />
+    </div>
   );
 }
